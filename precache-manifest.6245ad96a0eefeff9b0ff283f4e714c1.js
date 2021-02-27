2c239685a02c4caa2ac2d327590e6b00aec956ed
Evgeny Kuzyakov <ek@nearprotocol.com>
2021-02-27 15:50:18 -0800

    Updates

---
+++ precache-manifest.6245ad96a0eefeff9b0ff283f4e714c1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00da199e7c2a9fa807d37f7d172ac3e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75bf441e6c14c9886a91",
+    "url": "/static/css/2.f747b8b9.chunk.css"
+  },
+  {
+    "revision": "19223b0d58ff4d6f5cc0",
+    "url": "/static/css/main.ea237154.chunk.css"
+  },
+  {
+    "revision": "75bf441e6c14c9886a91",
+    "url": "/static/js/2.40b2a68a.chunk.js"
+  },
+  {
+    "revision": "a327c67d57be3bdec454207314626087",
+    "url": "/static/js/2.40b2a68a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19223b0d58ff4d6f5cc0",
+    "url": "/static/js/main.284b49da.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "f18e89c9f1e9b7080559da5bc2926895",
+    "url": "/static/media/broken-card.f18e89c9.png"
+  },
+  {
+    "revision": "f18e89c9f1e9b7080559da5bc2926895",
+    "url": "/static/media/logo.f18e89c9.png"
+  }
+]);
